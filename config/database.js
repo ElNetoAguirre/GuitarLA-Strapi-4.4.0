4dfbc7e6d9db015e763ae68cb81e1d1ed9d12b68
Ernesto Aguirre <ernesto_aguirre@hotmail.com>
2023-01-06 12:19:47 -0600

    Actualización de credenciales para la base de datos

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-ccqd0fpa6gdj00lf4vbg-a.oregon-postgres.render.com'),
+      host: env('DATABASE_HOST', 'dpg-cerhu9ha6gdlh2ubl2l0-a.oregon-postgres.render.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'guitarla_qod3'),
+      database: env('DATABASE_NAME', 'guitarla_iepb'),
       user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'NuMBA43WXMdjr5K5ZchM1L8x8yo9Recl'),
+      password: env('DATABASE_PASSWORD', 'tx1KCT05PL6swzg1EuMe3rxbt5YooOEn'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
